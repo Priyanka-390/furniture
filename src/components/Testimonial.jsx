8a058d25fdd953ad30570ecd8f90d05f177e9cef
Priyanka-390 <psr82220@gmail.com>
2024-01-19 16:05:23 +0530

    sidebar

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -9,6 +9,13 @@ import custmer4 from "../assets/images/testimonial4.webp";
 import custmer5 from "../assets/images/testimonial5.webp";
 import custmer6 from "../assets/images/testimonial6.webp";
 import custmer7 from "../assets/images/testimonial7.webp";
+import test1 from "../assets/images/test1.webp";
+import test2 from "../assets/images/test2.webp";
+import test3 from "../assets/images/test3.webp";
+import test4 from "../assets/images/test4.webp";
+import test5 from "../assets/images/test5.webp";
+import test6 from "../assets/images/test6.webp";
+import test7 from "../assets/images/test7.webp";
 const Testimonial = () => {
   var settings = {
     centerMode: true,
@@ -62,56 +69,98 @@ const Testimonial = () => {
               <img
                 src={custmer1}
                 alt="custumer"
-                className="flex w-full justify-center items-center max-w-[264px] "
+                className=" w-full justify-center items-center max-w-[264px] !hidden sm:!flex"
+              />
+              <img
+                src={test1}
+                alt="img1"
+                className="!flex sm:!hidden justify-center items-center max-w-[264px]"
               />
             </div>
             <div className=" !flex justify-center  items-center text-center mx-auto">
-              <img
+             <img
                 src={custmer2}
                 alt="custumer"
-                className="flex w-full justify-center items-center max-w-[164px] "
+                className="w-full justify-center items-center max-w-[164px] !hidden sm:!flex"
+              />
+              <img
+                src={test2}
+                alt="img1"
+                className="!flex sm:!hidden justify-center items-center max-w-[164px]"
               />
             </div>
             <div className=" !flex justify-center  items-center text-center mx-auto">
-              <img
+             <img
                 src={custmer3}
                 alt="custumer"
-                className="flex w-full justify-center items-center max-w-[148px] "
+                className="!hidden sm:!flex justify-center w-full items-center max-w-[148px] "
+              />
+              <img
+                src={test3}
+                alt="img"
+                className="!flex sm:!hidden justify-center items-center max-w-[148px]"
               />
             </div>
             <div className=" !flex justify-center  items-center text-center mx-auto">
-              <img
+             <img
                 src={custmer4}
                 alt="custumer"
-                className="flex w-full justify-center items-center max-w-[260px] "
+                className="!hidden sm:!flex w-full justify-center items-center max-w-[260px] "
+              />
+              <img
+                src={test4}
+                alt="img"
+                className="!flex sm:!hidden justify-center items-center max-w-[260px]"
               />
             </div>
             <div className=" !flex justify-center  items-center text-center mx-auto">
-              <img
+             <img
                 src={custmer5}
                 alt="custumer"
-                className="flex w-full justify-center items-center max-w-[258px] "
+                className="!hidden sm:!flex justify-center w-full items-center"
+              />
+              <img
+                src={test5}
+                alt="img"
+                className="!flex sm:!hidden justify-center items-center
+              "
               />
             </div>
             <div className=" !flex justify-center  items-center text-center mx-auto">
-              <img
+             <img
                 src={custmer6}
                 alt="custumer"
-                className="flex w-full justify-center items-center max-w-[193px] "
+                className="sm:!flex !hidden w-full justify-center items-center max-w-[193px] "
+              />
+              <img
+                src={test6}
+                alt="img"
+                className="!flex sm:!hidden justify-center items-center max-w-[193px]"
               />
             </div>
             <div className=" !flex justify-center  items-center text-center mx-auto">
-              <img
+            <img
                 src={custmer7}
                 alt="custumer"
-                className="flex w-full justify-center items-center max-w-[181px] "
+                className="sm:!flex !hidden w-full justify-center items-center max-w-[181px] "
+              />
+              <img
+                src={test7}
+                alt="img"
+                className="!flex sm:!hidden justify-center items-center max-w-[181px]"
               />
             </div>
             <div className=" !flex justify-center  items-center text-center mx-auto">
               <img
                 src={custmer5}
                 alt="custumer"
-                className="flex w-full justify-center items-center max-w-[258px] "
+                className="!hidden sm:!flex justify-center w-full items-center"
+              />
+              <img
+                src={test5}
+                alt="img"
+                className="!flex sm:!hidden justify-center items-center
+              "
               />
             </div>
           </Slider>
@@ -126,13 +175,22 @@ const Testimonial = () => {
           >
             <div>
               <div data-aos="zoom-in-down">
-                <h3 className=" text-center text-black font-semibold text-lg leading-normal font-poppins" data-aos="zoom-in-left">
+                <h3
+                  className=" text-center text-black font-semibold text-lg leading-normal font-poppins"
+                  data-aos="zoom-in-left"
+                >
                   Treepi Shoty
                 </h3>
-                <p className=" text-center text-[#4D4D4D] font-poppins text-sm font-light leading-noraml" data-aos="zoom-in-left">
+                <p
+                  className=" text-center text-[#4D4D4D] font-poppins text-sm font-light leading-noraml"
+                  data-aos="zoom-in-left"
+                >
                   Teacher
                 </p>
-                <p className="text-center max-w-[350px] text-black font-poppins text-sm font-light leading-normal pt-3 mx-auto" data-aos="zoom-in-left">
+                <p
+                  className="text-center max-w-[350px] text-black font-poppins text-sm font-light leading-normal pt-3 mx-auto"
+                  data-aos="zoom-in-left"
+                >
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                   Suspendisse feugiat nunc in leo vestibulum, at congue erat
                   accumsan. Suspendisse potenti. Suspendisse vitae vestibulum
